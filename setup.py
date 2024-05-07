a85abb482b788fee7739b25774670b9f712a28d9
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2024-05-07 14:19:00 +0530

    setup.py code added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Bot',
+    version= '0.0.0',
+    author= 'MohammedShaneeb',
+    author_email= 'shaneebkottakkal@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
