70da474ffd1fb4d0f67d1e7ff266ee2ea2795f3d
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2024-05-07 14:23:33 +0530

    helper edited

--- src/helper.py
+++ src/helper.py
@@ -1,13 +1,5 @@
-import bs4
-from langchain_chroma import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.llms import CTransformers
 from langchain.document_loaders import DirectoryLoader,PyPDFLoader
-from langchain_chroma import Chroma
-from langchain_core.prompts import PromptTemplate
-from langchain_pinecone import PineconeVectorStore
 from langchain.embeddings import HuggingFaceEmbeddings
 
 
